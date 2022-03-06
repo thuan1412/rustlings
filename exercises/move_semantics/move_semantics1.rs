c38810e76d85bf9ecde33f5f6bbf07a74858cea1
Thuấn Đỗ Hoàng <thuan.dh@teko.vn>
2022-03-06 12:25:48 +0700

    finish variables and functions exercises

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -6,7 +6,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let vec1 = fill_vec(vec0);
+    let vec1 = fill_vec(&vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -15,7 +15,7 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: Vec<i32>) -> &Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
